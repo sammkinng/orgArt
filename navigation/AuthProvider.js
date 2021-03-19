b446dbfe8e48158f99fffca41b2f3984e9595fa9
sammkinng <fm568pk@gmail.com>
2021-03-19 15:13:11 +0530

    make it better

--- navigation/AuthProvider.js
+++ navigation/AuthProvider.js
@@ -2,6 +2,7 @@ import React ,{ createContext } from "react";
 import { useState } from "react";
 import auth from '@react-native-firebase/auth';
 import { GoogleSignin } from "@react-native-community/google-signin";
+import { LoginManager, AccessToken } from 'react-native-fbsdk';
 
 export const AuthContext=createContext();
 
@@ -26,6 +27,31 @@ export const AuthProvider=({children})=>{
                         await auth().signInWithCredential(googleCredential);
                     } catch (e) {
                         console.log(e);
+                    }
+                },
+                fbLogin:async()=>{
+                    try {
+                        const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
+
+                        if (result.isCancelled) {
+                          throw 'User cancelled the login process';
+                        }
+                      
+                        // Once signed in, get the users AccesToken
+                        const data = await AccessToken.getCurrentAccessToken();
+                      
+                        if (!data) {
+                          throw 'Something went wrong obtaining access token';
+                        }
+                      
+                        // Create a Firebase credential with the AccessToken
+                        const facebookCredential = auth.FacebookAuthProvider.credential(data.accessToken);
+                      
+                        // Sign-in the user with the credential
+                        await auth().signInWithCredential(facebookCredential);
+                      
+                    } catch (error) {
+                        console.log(error);
                     }
                 },
                 register:async(email,password)=>{
